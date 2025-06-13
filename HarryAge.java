864ae243a09f2d362c8ba4e3801edcfffbbd8814
Mandavi Singh <112054358+mandavii@users.noreply.github.com>
2025-06-13 10:12:33 +0530

    Add files via upload

---
+++ HarryAge.java
@@ -0,0 +1,12 @@
+package Java_Programming_Element;
+
+public class HarryAge {
+
+	public static void main(String[] args) {
+		int birthYear = 2000;
+		int currentYear = 2024;
+		int harryAge = currentYear - birthYear;
+
+		System.out.println("Harry's age in 2024 is " + harryAge);
+	}
+}
