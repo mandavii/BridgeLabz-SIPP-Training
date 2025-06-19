d4a0f8917586ff6a3457a578df50552284135b97
Mandavi Singh <112054358+mandavii@users.noreply.github.com>
2025-06-19 12:29:31 +0530

    Update YoungestOf3.java

--- Control Flow/YoungestOf3.java
+++ Control Flow/YoungestOf3.java
@@ -7,29 +7,29 @@ public class YoungestOf3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Declare variables for ages and heights
+        
         int amarAge, akbarAge, anthonyAge;
         double amarHeight, akbarHeight, anthonyHeight;
 
-        // Get input for Amar
+    
         System.out.print("Enter Amar's age: ");
         amarAge = input.nextInt();
         System.out.print("Enter Amar's height (in cm): ");
         amarHeight = input.nextDouble();
 
-        // Get input for Akbar
+    
         System.out.print("Enter Akbar's age: ");
         akbarAge = input.nextInt();
         System.out.print("Enter Akbar's height (in cm): ");
         akbarHeight = input.nextDouble();
 
-        // Get input for Anthony
+        
         System.out.print("Enter Anthony's age: ");
         anthonyAge = input.nextInt();
         System.out.print("Enter Anthony's height (in cm): ");
         anthonyHeight = input.nextDouble();
 
-        // Find the youngest friend
+        
         int youngestAge = amarAge;
         String youngestFriend = "Amar";
 
@@ -42,7 +42,7 @@ public class YoungestOf3 {
             youngestFriend = "Anthony";
         }
 
-        // Find the tallest friend
+    
         double tallestHeight = amarHeight;
         String tallestFriend = "Amar";
 
@@ -55,8 +55,7 @@ public class YoungestOf3 {
             tallestFriend = "Anthony";
         }
 
-        // Display the results
-        System.out.println("\n--- Results ---");
+        
         System.out.println("The youngest friend is " + youngestFriend + " (Age: " + youngestAge + ").");
         System.out.println("The tallest friend is " + tallestFriend + " (Height: " + tallestHeight + " cm).");
 
