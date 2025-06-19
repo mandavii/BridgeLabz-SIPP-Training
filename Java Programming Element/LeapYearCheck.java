250f3ca9e3d94b8605f01fe00530ed3167322e19
Mandavi Singh <112054358+mandavii@users.noreply.github.com>
2025-06-19 11:18:53 +0530

    Add files via upload

---
+++ Java Programming Element/LeapYearCheck.java
@@ -0,0 +1,28 @@
+package Level3;
+import java.util.Scanner;
+
+public class LeapYearCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if (year >= 1582) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println("Leap Year");
+                    } else {
+                        System.out.println("Not a Leap Year");
+                    }
+                } else {
+                    System.out.println("Leap Year");
+                }
+            } else {
+                System.out.println("Not a Leap Year");
+            }
+        } else {
+            System.out.println("Year not valid (must be >= 1582)");
+        }
+    }
+}
